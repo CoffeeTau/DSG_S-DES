24e1fcf7d28297c051bf9eca2c3155f135544bb2
huangtao <wongtaut@qq.com>
2024-10-02 21:00:46 +0800

    str and readme

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 ![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen)
 
 # 关卡测试报告【共5关】
+## 第一关-基本测试
+## 第二关-交叉测试
+## 第三关-扩展功能
+## 第四关-暴力破解
+## 第五关-封闭测试
 
-# 用户指南
+# 算法相关统计分析
 
 # 开发手册
+
+# 用户指南
 
 # 常见问题
 
--- SDES.py
+++ SDES.py
@@ -14,6 +14,19 @@ class SDES:
         self.function_f = Function_f()
         self.subkeygenerator = SubKeyGenerator()
 
+    # 字符串填充
+    def padString(self, s):
+        # 计算需要填充的字节数
+        padding_length = 8 - (len(s) % 8)
+        if padding_length == 0:
+            padding_length = 8
+        padding_char = chr(padding_length).encode('utf - 8')
+        return s.encode('utf - 8') + padding_char * padding_length
+
+    def unpadString(self, s):
+        padding_length = s[-1]
+        return s[: - padding_length].decode('utf - 8')
+
     # type == 'E' : 该过程为加密过程
     # type == 'D' : 该过程为解密过程
     def encryptOrDecrypt(self, text, key, type):
@@ -49,10 +62,60 @@ class SDES:
 
         return text
 
+    def encryptString(self, s, key):
+        padded_s = self.padString(s)
+        encrypted_array = []
+        for i in range(0, len(padded_s), 8):
+            block = np.array(list(padded_s[i:i + 8]))
+            encrypted_block = self.encryptOrDecrypt(block, key, 'E')
+            encrypted_array.extend(encrypted_block)
+        return bytes(encrypted_array).hex()
+
+    def decryptString(self, s_hex, key):
+        s = bytes.fromhex(s_hex)
+        decrypted_array = []
+        for i in range(0, len(s), 8):
+            block = np.array(list(s[i:i + 8]))
+            decrypted_block = self.encryptOrDecrypt(block, key, 'D')
+            decrypted_array.extend(decrypted_block)
+        return self.unpadString(bytes(decrypted_array))
+
+    def encryptString(self, s, key):
+        binary_s = ''.join(format(ord(c), 'b').zfill(8) for c in s)
+        encrypted_binary = ''
+        for i in range(0, len(binary_s), 8):
+            block = np.array(list(map(int, list(binary_s[i:i + 8]))), dtype=np.uint8)
+            encrypted_block = self.encryptOrDecrypt(block, key, 'E')
+            encrypted_binary += ''.join(str(b) for b in encrypted_block)
+        encrypted_chars = []
+        for i in range(0, len(encrypted_binary), 8):
+            num = int(encrypted_binary[i:i + 8], 2)
+            encrypted_chars.append(chr(num))
+        return ''.join(encrypted_chars)
+
+    def decryptString(self, s, key):
+        binary_s = ''.join(format(ord(c), 'b').zfill(8) for c in s)
+        decrypted_binary = ''
+        for i in range(0, len(binary_s), 8):
+            block = np.array(list(map(int, list(binary_s[i:i + 8]))), dtype=np.uint8)
+            decrypted_block = self.encryptOrDecrypt(block, key, 'D')
+            decrypted_binary += ''.join(str(b) for b in decrypted_block)
+        decrypted_chars = []
+        for i in range(0, len(decrypted_binary), 8):
+            num = int(decrypted_binary[i:i + 8], 2)
+            decrypted_chars.append(chr(num))
+        return ''.join(decrypted_chars)
+
 if __name__ == "__main__":
     p = np.array([0, 0, 0, 0, 0, 0, 0, 0])
     key = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
     # c = np.array([1,1,1,0,1,0,1,1])
     sdes = SDES()
     p = sdes.encryptOrDecrypt(p, key, 'E')
-    print("密文:", p)
+    print("bit密文:", p)
+
+    p_str = "Default Security Group"
+    encrypted_result = sdes.encryptString(p_str, key)
+    print("字符串密文:", encrypted_result)
+    decrypted_result = sdes.decryptString(encrypted_result, key)
+    print("字符串明文:", decrypted_result)
